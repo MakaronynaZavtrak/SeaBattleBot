712627743a1e40b1add7dde05dbeecb42bfd0eb4
MakaronynaZavtrak <semyonov.owa@gmail.com>
2024-09-10 21:23:27 +0500

    Added first commit

---
+++ src/main/java/org/University/Main.java
@@ -0,0 +1,35 @@
+package org.University;
+
+import org.University.bot.TelegramBot;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class Main {
+    public static Properties getBotProperties(File propertyFile) throws IOException {
+        Properties properties = new Properties();
+        FileInputStream input = new FileInputStream(propertyFile);
+        properties.load(input);
+        return properties;
+    }
+    public static void main(String[] args) throws TelegramApiException {
+        String token;
+        String botUserName;
+                try {
+            Properties properties = getBotProperties(new File(args[0]));
+            token = properties.getProperty("bot.token");
+            botUserName = properties.getProperty("bot.name");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println(token);
+        System.out.println(botUserName);
+        TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
+        telegramBotsApi.registerBot(new TelegramBot(botUserName, token));
+    }
+}
---
+++ src/main/java/org/University/bot/TelegramBot.java
@@ -0,0 +1,56 @@
+package org.University.bot;
+
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Message;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.User;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+public class TelegramBot extends TelegramLongPollingBot {
+    private String botUserName;
+    private String token;
+
+    public TelegramBot(String botUserName, String token)
+    {
+        this.botUserName = botUserName;
+        this.token = token;
+    }
+    @Override
+    public void onUpdateReceived(Update update) {
+        Message message = update.getMessage();
+        User user = message.getFrom();
+        String text = message.getText();
+        Long chatId = user.getId();
+        switch (text)
+        {
+            case "start":
+                sendMessage(chatId, "Привет! Я эхо-бот, который пишет " +
+                    "сообщения в обратном порядке.");
+                break;
+            default:
+                sendMessage(chatId, new StringBuilder(text).reverse().toString());
+                break;
+        }
+
+    }
+
+    public void sendMessage(Long chatId, String whatToSend)
+    {
+        SendMessage message = new SendMessage();
+        message.setChatId(chatId);
+        message.setText(whatToSend);
+        try
+        {
+            execute(message);
+        }
+        catch (TelegramApiException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public String getBotUsername() {
+        return this.botUserName;
+    }
+}
