6623656de7eb42b2362b4efcd2613745e5a0d2a4
Oleg Semenov <semyonov.owa@gmail.com>
2026-08-01 14:52:12 +0500

    refactor(app): заменил Main на @SpringBootApplication и убрал самодельные билдеры.

---
+++ src/main/java/org/urfu/semyonovowa/config/BotConfiguration.java
@@ -0,0 +1,50 @@
+package org.urfu.semyonovowa.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+import org.urfu.semyonovowa.bot.TelegramBot;
+import org.urfu.semyonovowa.dataBase.DataBaseHandler;
+
+import java.util.Properties;
+
+/**
+ * Собирает и регистрирует бины приложения средствами Spring вместо самодельных билдеров.
+ * Регистрация в Telegram пока ручная (через {@link TelegramBotsApi}); на этапе 7
+ * она уедет в официальный Spring Boot стартер telegrambots.
+ */
+@Configuration
+public class BotConfiguration
+{
+    @Bean
+    public DataBaseHandler dataBaseHandler(DatabaseProperties databaseProperties)
+    {
+        Properties credentials = new Properties();
+        credentials.setProperty("user", databaseProperties.user());
+        credentials.setProperty("password", databaseProperties.password());
+        return new DataBaseHandler(
+                databaseProperties.forName(),
+                databaseProperties.url(),
+                credentials);
+    }
+
+    @Bean
+    public TelegramBot telegramBot(BotProperties botProperties, DataBaseHandler dataBaseHandler)
+    {
+        return new TelegramBot(
+                botProperties.name(),
+                botProperties.token(),
+                botProperties.creatorChatId(),
+                dataBaseHandler);
+    }
+
+    @Bean
+    public TelegramBotsApi telegramBotsApi(TelegramBot telegramBot) throws TelegramApiException
+    {
+        TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
+        telegramBotsApi.registerBot(telegramBot);
+        return telegramBotsApi;
+    }
+}
